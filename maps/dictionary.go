fb23a95861edb8072074c66522bdae71a76f0430
carlosHF9 <chenrique452@gmail.com>
2022-06-17 07:55:04 -0300

    maps dir added

---
+++ maps/dictionary.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"errors"
+)
+
+type Dictionary map[string]string
+
+var (
+	ErrNotFound = errors.New("could not find the word you were looking for")
+	ErrWordExists = errors.New("cannot add word because it already exists")
+)
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	
+	
+
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error{
+
+	d[word] = definition
+
+	return nil
+	
+}
